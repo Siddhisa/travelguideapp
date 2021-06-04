7c57e78074a9fc3869eabeefa82d23f3fb6f3e88
Siddhisa <siddhisaban@gmail.com>
2021-06-04 12:53:06 +0530

    Travel Guide App

---
+++ app/src/main/java/com/appsnipp/webviewsample/error_page.java
@@ -0,0 +1,13 @@
+package com.appsnipp.webviewsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class error_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error_page);
+    }
+}
